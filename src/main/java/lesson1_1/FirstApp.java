2025b88c4d2ff8ad65ede57e8710081654e13214
DMITRII <jd2344430@gmail.com>
2021-12-26 16:51:43 +0500

    lesson1

---
+++ src/main/java/lesson1_1/FirstApp.java
@@ -0,0 +1,19 @@
+package lesson1_1;
+
+public class FirstApp {
+    public static void main(String[] args) {
+        System.out.println("Hello java");
+        drawCat();
+    }
+    public static void drawCat(){
+        System.out.println("           _..---...,\"\"-._     ,/}/)\n" +
+                "        .''        ,      ``..'(/-<\n" +
+                "       /   _      {      )         \\\n" +
+                "      ;   _ `.     `.   <         a(\n" +
+                "    ,'   ( \\  )      `.  \\ __.._ .: y\n" +
+                "   (  <\\_-) )'-.____...\\  `._   //-'\n" +
+                "    `. `-' /-._)))      `-._)))\n" +
+                "      `...'         ");
+
+    }
+}
