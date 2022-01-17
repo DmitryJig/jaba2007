06e906c6c68185e17bb50dd27a4ba829043b2850
DMITRII <jd2344430@gmail.com>
2022-01-18 02:36:27 +0500

    Домашняя работа 4

--- src/main/java/course1/lesson4/HomeWork_4.java
+++ src/main/java/course1/lesson4/HomeWork_4.java
@@ -8,11 +8,11 @@ public class HomeWork_4 {
     //поле
     public static char[][] map;
 
-    //размер поля
-    public static final int SIZE = 3;
+    //размер поля любое целое положительное число
+    public static final int SIZE = 5;
 
-    //точек для победы
-    public static final int DOTS_TO_WIN = 3;
+    //точек для победы любое целое положительное число меньшее или равно SIZE
+    public static final int DOTS_TO_WIN = 4;
 
     //описание точек
     public static final char DOT_EMPTY = '*';
@@ -65,6 +65,7 @@ public class HomeWork_4 {
             y = SCANNER.nextInt() - 1;
         } while (!isCellValid(x, y));
         map[y][x] = DOT_X;
+        System.out.println("Вы сходили в точку " + (x + 1) + " " + (y + 1));
     }
 
     /**
@@ -111,18 +112,45 @@ public class HomeWork_4 {
             for (int j = 0; j < SIZE; j++) {
                 dotCountX = map[i][j] == symbol ? ++dotCountX : 0;
                 dotCountY = map[j][i] == symbol ? ++dotCountY : 0;
-                if (dotCountX >= DOTS_TO_WIN || dotCountY >=DOTS_TO_WIN) {
+                if (dotCountX >= DOTS_TO_WIN || dotCountY >= DOTS_TO_WIN) {
                     return true;
                 }
             }
         }
 
-        // диагонали
-        if (map[0][0] == symbol && map[1][1] == symbol && map[2][2] == symbol) {
-            return true;
+        // диагональ слева направо сверху вниз
+
+        int countPositions = SIZE - DOTS_TO_WIN + 1; // количество линий, по которым можно выиграть на одной оси
+
+        for (int startPos = 0; startPos < countPositions; startPos++) { // стартовая позиция счетчика
+            int j = 0;
+            int dotCount1 = 0; // счетчик точек слева направо сверху вниз на диагонали и ниже
+            int dotCount2 = 0; // счетчик точек слева направо сверху вниз на диагонали и выше
+            for (int i = startPos; i < SIZE; i++) {
+                dotCount1 = map[i][j] == symbol ? ++dotCount1 : 0;
+                dotCount2 = map[j][i] == symbol ? ++dotCount2 : 0;
+                if (dotCount1 >= DOTS_TO_WIN || dotCount2 >= DOTS_TO_WIN) {
+                    return true;
+                }
+                j++;
+            }
+
         }
-        if (map[0][2] == symbol && map[1][1] == symbol && map[2][0] == symbol) {
-            return true;
+
+        // диагональ слева направо снизу вверх
+
+        for (int startPos = SIZE - 1; startPos >= (SIZE - countPositions); startPos--) {
+            int j = 0;
+            int dotCount1 = 0;
+            int dotCount2 = 0;
+            for (int i = startPos; i >= 0; i--) {
+                dotCount1 = map[i][j] == symbol ? ++dotCount1 : 0;
+                dotCount2 = map[SIZE - j - 1][SIZE - i - 1] == symbol ? ++dotCount2 : 0;
+                if (dotCount1 >= DOTS_TO_WIN || dotCount2 >= DOTS_TO_WIN) {
+                    return true;
+                }
+                j++;
+            }
         }
 
         return false;
