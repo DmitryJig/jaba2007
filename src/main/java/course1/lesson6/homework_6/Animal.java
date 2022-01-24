b19ba039a9c841948f12e27ae4572cb33d875eac
DMITRII <jd2344430@gmail.com>
2022-01-24 22:31:42 +0500

    Домашняя работа 6

--- src/main/java/course1/lesson6/homework_6/Animal.java
+++ src/main/java/course1/lesson6/homework_6/Animal.java
@@ -3,7 +3,7 @@ package course1.lesson6.homework_6;
 /**
  * Суперкласс наследниками которого будут классы Dog и Cat
  */
-public class Animal {
+public abstract class Animal {
     protected final int DIST_RUN;
     protected final int DIST_SWIM;
     String name;
@@ -14,21 +14,7 @@ public class Animal {
         this.name = name;
     }
 
-    public void run(int distance) {
+    public abstract void run(int distance);
 
-        if (distance <= DIST_RUN) {
-            System.out.println("Животное " + name + " пробежало " + distance + " м.");
-        } else {
-            System.out.println("Животное " + name + " не добежало, батарейка села");
-        }
-    }
-
-    public void swim(int distance) {
-
-        if (distance <= DIST_SWIM) {
-            System.out.println("Животное " + name + " проплыло " + distance + " м.");
-        } else {
-            System.out.println("Животное " + name + " утонуло");
-        }
-    }
+    public abstract void swim(int distance);
 }
