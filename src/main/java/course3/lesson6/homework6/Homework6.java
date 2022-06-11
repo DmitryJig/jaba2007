1e4853a8df800b5c17ff9e85e856fe90f9c49bde
dima <jd2344430@gmail.com>
2022-06-11 22:42:44 +0500

    j3_ hw6_02

--- src/main/java/course3/lesson6/homework6/Homework6.java
+++ src/main/java/course3/lesson6/homework6/Homework6.java
@@ -22,6 +22,7 @@ public class Homework6 {
         for (int i = len - 1; i >= 0; i--) {
             if (array[i] == number){
                 positionAfterNumber = i + 1;
+                break;
             }
         }
         if (positionAfterNumber == -1){
@@ -36,6 +37,19 @@ public class Homework6 {
         return result;
     }
 
+    /**
+     * Написать метод, который проверяет состав массива из чисел 1 и 4. Если в нем нет хоть одной
+     * четверки или единицы, то метод вернет false; Написать набор тестов для этого метода (по 3-4
+     * варианта входных данных).
+     * [ 1 1 1 4 4 1 4 4 ] -> true
+     * [ 1 1 1 1 1 1 ] -> false
+     * [ 4 4 4 4 ] -> false
+     * [ 1 4 4 1 1 4 3 ] -> false
+     * @param array - входящий массив
+     * @param num1 первое число ( 1 )
+     * @param num2 второе число ( 4 )
+     * @return
+     */
     public boolean checkAvailable1And4(int[] array, int num1, int num2){
         List<Integer> list = IntStream.of(array).boxed().toList();
         if(list.stream().anyMatch(x -> x != num1 && x!= num2) || !list.contains(num1) || !list.contains(num2)){
--- src/test/java/course3/lesson6/homework6/Homework6Test.java
+++ src/test/java/course3/lesson6/homework6/Homework6Test.java
@@ -1,17 +1,18 @@
 package course3.lesson6.homework6;
 
+import org.hamcrest.MatcherAssert;
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.CsvFileSource;
-import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.MethodSource;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.stream.Stream;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 class Homework6Test {
 
@@ -22,16 +23,36 @@ class Homework6Test {
         homework6 = new Homework6();
     }
 
-    @Test // тест без 4
-    void testGetNumbersAfterLastNumberWithException() {
+
+    // Тесты первого метода
+    @Test
+    void testGetNumbersAfterLastNumber() {
+        int[] testArray = {Integer.MAX_VALUE, 0, Integer.MIN_VALUE, 4, 0};
+        int number = 4;
+        Assertions.assertArrayEquals(new int[]{0}, homework6.getNumbersAfterLastNumber(testArray, number));
+    }
+
+    @Test
+    public void testGetNumbersAfterLastNumberWithMatchers() {
+        int[] array = {1, 2, 4, 4, 2, 3, 4, 1, 7};
+        int[] res = homework6.getNumbersAfterLastNumber(array, 4);
+        int[] expected = {1, 7};
+
+        MatcherAssert.assertThat(res, Matchers.is(expected));
+    }
+
+    @Test // тест без 4, проверяем что метод бросает исключение
+    public void testGetNumbersAfterLastNumberWithException() {
         int[] testArray = {1, 2, 5,7, 9};
-        Assertions.assertThrows(RuntimeException.class, ()-> homework6.getNumbersAfterLastNumber(testArray, 4));
+        int number = 4;
+        Assertions.assertThrows(RuntimeException.class, ()-> homework6.getNumbersAfterLastNumber(testArray, number));
     }
 
     @ParameterizedTest
     @MethodSource("provideArrayForLastNumbers")
     public void testGetNumbersAfterLastNumberParametrized(int[] inputArray, int[] expectedArray){
-        int[] arrayResult = homework6.getNumbersAfterLastNumber(inputArray, 4);
+        int number = 4;
+        int[] arrayResult = homework6.getNumbersAfterLastNumber(inputArray, number);
         Assertions.assertArrayEquals(expectedArray, arrayResult);
     }
 
@@ -45,9 +66,34 @@ class Homework6Test {
         );
     }
 
+
+    // Тесты второго метода
+    @Test
+    public void testCheckAvailable1And4(){
+        int[] array = {1, 1, 1, 4, 4, 1, 4, 4};
+        Assertions.assertTrue(homework6.checkAvailable1And4(array, 1, 4));
+    }
+
+    @Test
+    public void testCheckAvailable1And4WithMatchers(){
+        int[] array = {1, 1, Integer.MAX_VALUE, 4, 4, 1, 4, Integer.MIN_VALUE};
+        boolean res = homework6.checkAvailable1And4(array, 1, 4);
+        MatcherAssert.assertThat(true, Matchers.not(res));
+    }
+
+
+    /**
+     * Метод проверяет неверные комбинации цифр в массиве (на выдачу false)
+     * из файла-источника
+     * Не смог найти как принять в параметры метода массив, по этому принимаем строку и парсим ее
+     * @param numbers строка с цифрами разделенными пробелами
+     */
     @ParameterizedTest
-    @CsvFileSource(resources = "input")
-    void testCheckAvailable1And4Parametrized() {
+    @CsvFileSource(resources = "inputData.csv", numLinesToSkip = 1)
+    void testCheckAvailable1And4Parametrized(String numbers) {
+        // Парсим строку в массив целых чисел
+        int[] arr = Arrays.stream(numbers.split("\\p{Zs}")).mapToInt(n -> Integer.parseInt(n)).toArray();
+        Assertions.assertFalse(homework6.checkAvailable1And4(arr, 1, 4));
     }
 
 
